0d18db61c63ef649b99b3249a6380300311712ae
çağatay güven <cagatayguven9@gmail.com>
2022-06-06 00:50:47 +0300

    26th lessons

---
+++ C kursu/26.Ders/nutility.h
@@ -0,0 +1,19 @@
+#ifndef NUTILITY_H
+#define NUTILITY_H
+
+
+
+
+#define isleap(y)  ((y) % 4 == 0 && ((y) % 100 != 0 || (y) % 400 == 0)) 
+#define randomize() srand((unsigned)time(NULL))
+
+int    isprime(int val);
+void   pline(void);
+int    ndigit(int);
+
+void set_array_random(int* p, int size);
+void print_array(const int* p, int size);
+
+#include "nutility.c"
+
+#endif
